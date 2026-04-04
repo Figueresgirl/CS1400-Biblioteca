42038306a9c975be9b7bcfad70883f4c0abe9868
Figueresgirl <thaismedina@mail.weber.edu>
2026-04-03 19:21:27 -0700

    listo

--- M12/mago_meteorologico_m12/src/weather_wizard/main.py
+++ M12/mago_meteorologico_m12/src/weather_wizard/main.py
@@ -6,20 +6,16 @@ from weather_wizard.motor import obtener_clima_ciudad
 
 
 def main():
-    try:
-        ciudad = input("Introduce una ciudad: ")
-        resultado = obtener_clima_ciudad(ciudad)
+    ciudad = input("Introduce una ciudad: ").strip()
+    resultado = obtener_clima_ciudad(ciudad)
 
-        if "error" in resultado:
-            print("Error:", resultado["error"])
-        else:
-            print("\nClima actual:")
-            print(f"Ciudad: {resultado['city']}")
-            print(f"Temperatura: {resultado['temp']}°C")
-            print(f"Condición: {resultado['condition']}")
-
-    except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
+    if "error" in resultado:
+        print("Error:", resultado["error"])
+    else:
+        print("\nClima actual:")
+        print(f"Ciudad: {resultado['city']}")
+        print(f"Temperatura: {resultado['temp']}°C")
+        print(f"Condición: {resultado['condition']}")
 
 
 if __name__ == "__main__":
